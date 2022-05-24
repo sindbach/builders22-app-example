9b136509b657d37d485ce8fd2dd1916e2f6e1874
Wan Bachtiar <sindbach@gmail.com>
2022-05-24 21:01:24 +1000

    Added logging

--- app.go
+++ app.go
@@ -10,9 +10,19 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello there!")
 }
 
+func logRequest(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("%s %s %s\n", r.RemoteAddr, r.Method, r.URL)
+		handler.ServeHTTP(w, r)
+	})
+}
+
 func main() {
-	port := ":8080"
+	httpPort := ":8080"
+
+	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
+
 	http.HandleFunc("/api/v1/hello", handler)
-	fmt.Println("Server is running on port ", port)
-	log.Fatal(http.ListenAndServe(port, nil))
+	fmt.Println("Listening on port ", httpPort)
+	log.Fatal(http.ListenAndServe(httpPort, logRequest(http.DefaultServeMux)))
 }
